b11cb70edac4679c4a0f6fa28cd14866002b4b0d
GAJEWSKI Filip <fgajewski@europe.corp.altran.com>
2018-10-01 09:45:26 +0100

    First Commit - Pom created and SDK dependancy sorted. Uploating to remote

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import com.amazonaws.AmazonServiceException;
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import com.amazonaws.services.s3.model.S3Object;
+//import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
